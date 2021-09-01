1f83fc9e2cbc60b9f4bdbb141a8bf50aa49b5167
90houlaoheshang <907333918@qq.com>
2021-09-01 16:00:01 +0800

    适配测试

--- tests/main.py
+++ tests/main.py
@@ -1,6 +1,12 @@
+import asyncio
+
 import asyncUnittest
 from Gear_test import TestGear
-from method_run_when_test import TestInstance_run_when
+from Datashine_test import TestDataShine
+# from method_run_when_test import TestInstance_run_when
 from run_when_test import TestRunWhen
 
+import uvloop
+
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 asyncUnittest.run()
